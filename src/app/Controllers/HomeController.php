651390f460dd1678e00ee33c75a970c13776ecad
Arman Fayziev <arman.fayziev@scandiweb.com>
2022-12-08 02:57:33 +0200

    sql queries

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -19,6 +19,36 @@ class HomeController
             throw new \PDOException($e->getMessage(), $e->getCode());
         }
 
+        $email = 'jone.done@.com';
+        $name  = 'Jone Doe';
+        $is_active = 'true';
+        $created_at = '1/08/1987';
+
+        // $query = 'INSERT INTO users (email, full_name, is_active, created_at)
+        //            VALUES (?, ?, ?, ?)';
+        
+        $query = 'INSERT INTO users (email, full_name, is_active, created_at)
+        VALUES (:email, :name, :active, :date)';
+
+        $stmt = $db->prepare($query);
+
+        $stmt->execute(
+            [
+               'name' => $email,
+               'email'=> $name,
+               'active' => $is_active, 
+               'date' => $created_at
+            ]
+        );
+
+        $id = (int) $db->lastInsertId();
+
+        $user = $db->query('SELECT *FROM users WHERE id=' . $id)->fetch();
+
+        echo '<pre>';
+        var_dump($user);
+        echo '</pre>';
+
         var_dump($db);
         return View::make('index');
     }
